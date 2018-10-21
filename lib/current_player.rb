b712c6bffbae457cd73b97bab93d8312bacff207
Thuyan Duong <thuyan.duong@flatironschool.com>
2018-10-21 22:31:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,12 @@ def turn_count(board)
   turns
 end 
 
-def current_player
-  
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+    player = 'X'
+  else
+    player = 'O'
+  end
+  player
 end
